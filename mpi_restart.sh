5afb172cbc3fda9477affec0089d4efe14c401cf
rakurai <rakurai@node-0.rakurai-qv16971.bdslab-pg0.clemson.cloudlab.us>
2016-06-30 13:08:05 -0400

    scripts

---
+++ mpi_restart.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+bridge=br0
+
+docker kill mpi
+docker rm mpi
+docker run --net=none --name mpi -d rakurai/openmpi
+sudo pipework/pipework $bridge mpi 10.10.1.100/24
+
+for i in `seq 1 8`; do
+  ip=10.10.1.10$i
+  name=mpi-$i
+  ssh node-$i "docker kill mpi; docker rm mpi; docker pull rakurai/openmpi; docker run --net=none -h $name --name mpi -d rakurai/openmpi; sudo mpitest/pipework/pipework $bridge mpi $ip/24" &
+done
+
---
+++ setup_bridge.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+addr=`ifconfig|grep 10.10.1|tr ':' ' '|awk '{print $3}'`
+iface=`ifconfig | awk '/10.10.1/ {print $1}' RS="\n\n"`
+bridge=br0
+
+ip link add name $bridge type bridge
+ip link set $bridge up
+ip link set $iface master $bridge
+ifconfig $bridge $addr
+ifconfig $iface 0
