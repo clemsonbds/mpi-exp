0ea18fa794d16dcdcc713e7f56a1d2c6090e1462
rakurai <rakurai@node-0.rakurai-qv16971.bdslab-pg0.clemson.cloudlab.us>
2016-06-30 12:36:41 -0400

    fixed release name

--- install_docker.sh
+++ install_docker.sh
@@ -3,7 +3,8 @@
 apt-get update
 apt-get install -y apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
+release=$(lsb_release -sc)
+echo "deb https://apt.dockerproject.org/repo ubuntu-$release main" > /etc/apt/sources.list.d/docker.list
 
 apt-get update
 apt-cache policy docker-engine
