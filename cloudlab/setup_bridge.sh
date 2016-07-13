388cd5dad1605918544566b755b73ff1d12c7fbf
rakurai <rakurai@node-1.rakurai-qv17371.bdslab-pg0.wisc.cloudlab.us>
2016-07-13 14:07:50 -0500

    .

--- cloudlab/setup_bridge.sh
+++ cloudlab/setup_bridge.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 addr=`ifconfig|grep 10.10.1|tr ':' ' '|awk '{print $3}'`
-iface=`ifconfig | awk '/10.10.1/ {print $1}' RS="\n\n"`
+iface=`ifconfig|grep 90:e2:ba|awk '{print $1}'`
+#iface=`ifconfig | awk '/10.10.1/ {print $1}' RS="\n\n"`
 bridge=br0
 
 ip link add name $bridge type bridge
--- cloudlab/teardown_bridge_veth.sh
+++ cloudlab/teardown_bridge_veth.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 addr=`ifconfig|grep 10.10.1|tr ':' ' '|awk '{print $3}'`
-iface=`ifconfig | awk '/00:8c:fa/ {print $1}'`
+iface=`ifconfig|grep 90:e2:ba|awk '{print $1}'`
+#iface=`ifconfig | awk '/00:8c:fa/ {print $1}'`
 bridge=br0
 
 ip link del $bridge
