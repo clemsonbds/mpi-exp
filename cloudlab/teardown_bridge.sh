c2b950634cf9ed5f2830aaad5ce86f78acbd58c5
rakurai <rakurai@node-0.rakurai-qv17371.bdslab-pg0.wisc.cloudlab.us>
2016-07-13 14:56:37 -0500

    .

--- cloudlab/teardown_bridge.sh
+++ cloudlab/teardown_bridge.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 addr=`ifconfig|grep 10.10.1|tr ':' ' '|awk '{print $3}'`
-iface=`ifconfig | awk '/00:8c:fa/ {print $1}'`
+iface=`ifconfig|grep 90:e2:ba|awk '{print $1}'`
+#iface=`ifconfig | awk '/00:8c:fa/ {print $1}'`
 bridge=br0
 
-ip link delete $bridge type bridge
+ip link del $bridge
 ifconfig $iface $addr
